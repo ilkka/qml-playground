a91f059c4a204878553137e47df35d91234350b0
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-15 19:15:14 +0300

    Print message for registerTypes

--- TimeProvider/timeprovider_plugin.cpp
+++ TimeProvider/timeprovider_plugin.cpp
@@ -2,9 +2,11 @@
 #include "timeprovider.h"
 
 #include <QtDeclarative/qdeclarative.h>
+#include <QDebug>
 
 void TimeProviderPlugin::registerTypes(const char *uri)
 {
+    qDebug() << "TimeProviderPlugin registering types";
     qmlRegisterType<TimeProvider>(uri, 1, 0, "TimeProvider");
 }
 
