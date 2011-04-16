efed39d3e41f42bba53cc6a9abef6ef8124e83fb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-16 10:10:38 +0300

    Set non-visual flag false

--- Physics/particle.cpp
+++ Physics/particle.cpp
@@ -5,11 +5,7 @@
 Particle::Particle(QDeclarativeItem *parent):
         QDeclarativeItem(parent)
 {
-    // By default, QDeclarativeItem does not draw anything. If you subclass
-    // QDeclarativeItem to create a visual item, you will need to uncomment the
-    // following line:
-
-    // setFlag(ItemHasNoContents, false);
+    setFlag(ItemHasNoContents, false);
 }
 
 Particle::~Particle()
