12ca87222bc3116d969ea62aa17ede5513a13f62
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-16 10:32:51 +0300

    First implementation of Particle in C++
    Uses QVector2D for both position and velocity. I'm hoping that this will
    work okay? don't see major reasons why it should fail.

--- Physics/particle.cpp
+++ Physics/particle.cpp
@@ -1,9 +1,12 @@
 #include "particle.h"
 
 #include <QtDeclarative/qdeclarative.h>
+#include <QPainter>
 
-Particle::Particle(QDeclarativeItem *parent):
-        QDeclarativeItem(parent)
+Particle::Particle(QVector2D position, QVector2D velocity, QDeclarativeItem *parent):
+    QDeclarativeItem(parent),
+    m_position(position),
+    m_velocity(velocity)
 {
     setFlag(ItemHasNoContents, false);
 }
@@ -12,3 +15,23 @@ Particle::~Particle()
 {
 }
 
+void Particle::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+
+}
+
+QVector2D Particle::getPosition() const
+{
+    return m_position;
+}
+
+QVector2D Particle::getVelocity() const
+{
+    return m_velocity;
+}
+
+void Particle::move(qreal time_elapsed)
+{
+    m_position += m_velocity * time_elapsed;
+    emit positionChanged();
+}
--- Physics/particle.h
+++ Physics/particle.h
@@ -2,15 +2,51 @@
 #define PARTICLE_H
 
 #include <QtDeclarative/QDeclarativeItem>
+#include <QPoint>
+#include <QVector2D>
 
 class Particle : public QDeclarativeItem
 {
     Q_OBJECT
     Q_DISABLE_COPY(Particle)
+    Q_PROPERTY(QVector2D position READ getPosition NOTIFY positionChanged)
+    Q_PROPERTY(QVector2D velocity READ getVelocity NOTIFY velocityChanged)
 
 public:
-    Particle(QDeclarativeItem *parent = 0);
+    Particle(QVector2D position, QVector2D velocity, QDeclarativeItem *parent = 0);
     ~Particle();
+
+    /**
+     * Paint the particle.
+     * @overload QDeclarativeItem::paint().
+     */
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget = 0);
+
+    /**
+     * Get the particle position.
+     * @return the position of the particle.
+     */
+    QVector2D getPosition() const;
+
+    /**
+     * Get the velocity of the particle.
+     * @return the velocity of the particle.
+     */
+    QVector2D getVelocity() const;
+
+    /**
+     * Cause particle to move according to the amount of time that passed.
+     * @param time_elapsed number of seconds since last move.
+     */
+    void move(qreal time_elapsed);
+
+signals:
+    void positionChanged();
+    void velocityChanged();
+
+private:
+    QVector2D m_position;
+    QVector2D m_velocity;
 };
 
 QML_DECLARE_TYPE(Particle)
