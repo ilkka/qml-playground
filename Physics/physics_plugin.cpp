e0e416cea8ce99dcb44c449db8b913696bb0ba58
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-16 13:05:22 +0300

    Register Simulation type

--- Physics/physics_plugin.cpp
+++ Physics/physics_plugin.cpp
@@ -1,11 +1,13 @@
 #include "physics_plugin.h"
 #include "particle.h"
+#include "simulation.h"
 
 #include <QtDeclarative/qdeclarative.h>
 
 void PhysicsPlugin::registerTypes(const char *uri)
 {
     qmlRegisterType<Particle>(uri, 1, 0, "Particle");
+    qmlRegisterType<Simulation>(uri, 1, 0, "Simulation");
 }
 
 Q_EXPORT_PLUGIN2(Physics, PhysicsPlugin)
